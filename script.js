e403385ba276030057a5556f526a19754127a3e7
Pattonieri-L <pattonieri.luca@iisgalvanimi.edu.it>
2026-01-30 09:01:48 +0000

    punto4

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let conto = 0;
+  let votoAttuale = 0;
+        const Voto = document.getElementById('voto');
+
+        function aggiornaVoto(cambio) {
+            let nuovoVoto = votoAttuale + cambio;
+
+            
+            if (nuovoVoto > 10 || nuovoVoto < 0) {
+                alert("Limite raggiunto");
+                return;
+            }
+
+            votoAttuale = nuovoVoto;
+            displayVoto.innerText = votoAttuale;
+
+                  if (votoAttuale >= 6) {
+                Voto.className = 'verde';
+            } else {
+                Voto.className = 'rosso';
+
+            }
+          
+            }
+        
